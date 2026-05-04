babea19c455739034173394ebbf4511c97f6b0bb
laacin <lautarocingolani07@gmail.com>
2026-05-04 08:59:36 -0300

    chore: remove unused type names

--- intr/dialect/values.go
+++ intr/dialect/values.go
@@ -1,23 +1,4 @@
 package dialect
 
-// Simple values
-type ValueKind int
-
-// NOTE:: unused
-const (
-	ValueString ValueKind = iota
-	ValueNumber
-	ValueFloat
-	ValueBool
-	ValueNull
-	ValueParam
-
-	ValueTable
-	ValueColumn
-	ValueExpr
-)
-
-// --- Types
-
 // Param represents a value that will be stored and written as a dialect placeholder
 type Param struct{ Value any }
