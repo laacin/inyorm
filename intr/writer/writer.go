79300ad630175a12d8c58d08f3baa8903d8f3297
laacin <lautarocingolani07@gmail.com>
2026-05-04 08:56:32 -0300

    refactor: simplify (Writer).Value()

--- intr/writer/writer.go
+++ intr/writer/writer.go
@@ -7,12 +7,10 @@ import (
 )
 
 type Writer struct {
-	sb                 strings.Builder
-	ColumnWritingModes dialect.WritingModeConfig
-	TableWritingModes  dialect.WritingModeConfig
-	aliases            *AliasStore
-	dial               dialect.Dialect
-	params             *ParamStore
+	sb      strings.Builder
+	aliases *AliasStore
+	dial    dialect.Dialect
+	params  *ParamStore
 }
 
 func (w *Writer) Write(str string) {
@@ -23,28 +21,31 @@ func (w *Writer) Char(char byte) {
 	w.sb.WriteByte(char)
 }
 
-func (w *Writer) Value(v any, ctx dialect.ClauseName) {
+func (w *Writer) Value(v any, mode dialect.WritingMode) {
 	switch v := Normalize(v).(type) {
 	case dialect.Param:
 		w.params.Store(v)
 		w.dial.Placeholder(w, w.params.GetCount())
 
 	case dialect.Table:
-		isDef := obtainWriteMode(ctx, &w.TableWritingModes) == dialect.WriteDef
-		w.dial.Table(w, v, isDef)
+		w.dial.Table(w, v)
 
 	case dialect.Column:
-		switch obtainWriteMode(ctx, &w.ColumnWritingModes) {
+		switch mode {
 		case dialect.WriteBase:
 			w.dial.ColBase(w, v)
+
 		case dialect.WriteExpr:
 			w.dial.ColExpr(w, v)
+
 		case dialect.WriteAlias:
 			w.dial.ColAlias(w, v)
+
 		case dialect.WriteDef:
 			w.dial.ColDef(w, v)
+
 		default:
-			w.dial.Null(w)
+			w.dial.ColExpr(w, v)
 		}
 
 	case string:
@@ -63,7 +64,7 @@ func (w *Writer) Value(v any, ctx dialect.ClauseName) {
 
 func (w *Writer) GetTableRef(tbl string) (ref byte, shouldBeUsed bool) {
 	if w.aliases == nil {
-		return 'a', false
+		return ' ', false
 	}
 
 	return w.aliases.Get(tbl), true
@@ -75,48 +76,4 @@ func (w *Writer) Result() string {
 
 func (w *Writer) Reset() {
 	w.sb.Reset()
-}
-
-// -- Helpers
-func obtainWriteMode(ctx dialect.ClauseName, modes *dialect.ClauseWritingConfig) dialect.WritingMode {
-	switch ctx {
-	case dialect.ClauseNameInsertInto:
-		return modes.InsertInto
-
-	case dialect.ClauseNameSelect:
-		return modes.Select
-
-	case dialect.ClauseNameFrom:
-		return modes.From
-
-	case dialect.ClauseNameJoin:
-		return modes.Join
-
-	case dialect.ClauseNameWhere:
-		return modes.Where
-
-	case dialect.ClauseNameGroupBy:
-		return modes.GroupBy
-
-	case dialect.ClauseNameHaving:
-		return modes.Having
-
-	case dialect.ClauseNameOrderBy:
-		return modes.OrderBy
-
-	case dialect.ClauseNameLimit:
-		return modes.Limit
-
-	case dialect.ClauseNameOffset:
-		return modes.Offset
-
-	case dialect.ClauseNameUpdate:
-		return modes.Update
-
-	case dialect.ClauseNameDelete:
-		return modes.Delete
-
-	default:
-		return dialect.WriteBase
-	}
 }
