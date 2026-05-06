278c9e908d01a820553a2cde51bc53e2b6056a16
laacin <lautarocingolani07@gmail.com>
2026-05-06 09:31:57 -0300

    chore(entity): rename values.go => internal.go

--- internal/entity/internal.go
+++ internal/entity/internal.go
@@ -23,12 +23,46 @@ const (
 	ValueColumn
 )
 
+type ClauseKind int
+
+const (
+	// Select statement
+	ClauseSelect ClauseKind = iota
+	ClauseFrom
+	ClauseJoin
+	ClauseWhere
+	ClauseGroupBy
+	ClauseHaving
+	ClauseOrderBy
+	ClauseLimit
+	ClauseOffset
+
+	// Insert statement
+	ClauseInsertInto
+
+	// Update statement
+	ClauseUpdate
+
+	// Delete statement
+	ClauseDelete
+)
+
 type Value interface {
 	Kind() ValueKind
 	Write(Writer, ValueWriter, WritingMode)
 }
 
+type Clause interface {
+	Kind() ClauseKind
+	Write(Writer, ClauseWriter)
+}
+
 // Wrapper implemetations must implements this
 type ValueDefer interface {
 	Defer() Value
+}
+
+type ClauseDefer interface {
+	IsDeclared() bool
+	Defer() Clause
 }
