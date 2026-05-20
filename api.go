00eaaf74b1e82da0921a4b34eeeb5a4121bd81c4
laacin <lautarocingolani07@gmail.com>
2026-05-20 16:18:27 -0300

    chore: define API interfaces

---
+++ api.go
@@ -0,0 +1,46 @@
+package inyorm
+
+import "context"
+
+// --- Main input
+
+type InyormBuilder interface {
+	// Dml
+	Select(func(q SelectQuery, e Expr)) Statement
+	Insert(func(q InsertQuery, e Expr)) Statement
+	Update(func(q UpdateQuery, e Expr)) Statement
+	Delete(func(q DeleteQuery, e Expr)) Statement
+
+	// Execution
+	RunTx(context.Context, ...Runner) error
+}
+
+// --- Splits
+type Statement interface {
+	Runner
+	Prepare() PrepStatement
+	Bind(...any) Statement
+}
+
+type PrepStatement interface {
+	Bind(...any) PrepStatement
+	Values(...any) Runner
+}
+
+type Runner interface {
+	Run(...context.Context) error
+}
+
+// --- Queries
+
+type Expr interface {
+	Table(name string) Table
+	Col(name string, table ...string) Column
+	All(table ...string) Column
+	Lazy(id ...string) Parameter
+	Param(value ...any) Parameter
+	Cond(ident any) Condition
+	Concat(v ...any) Column
+	Switch(cond any, fn func(cs Case)) Column
+	Search(fn func(cs Case)) Column
+}
