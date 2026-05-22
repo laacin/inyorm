6204c2f15c3c4e60f35be1ea866c52de86fed4ab
laacin <lautarocingolani07@gmail.com>
2026-05-22 03:03:12 -0300

    feat: implements RunTx

--- inyorm.go
+++ inyorm.go
@@ -1,6 +1,10 @@
 package inyorm
 
 import (
+	"context"
+	"errors"
+
+	"github.com/laacin/inyorm/internal/core"
 	"github.com/laacin/inyorm/internal/impl/exprimpl"
 	"github.com/laacin/inyorm/internal/impl/statement"
 	"github.com/laacin/inyorm/internal/ir"
@@ -14,6 +18,31 @@ func New(eng *Engine) (*DB, error) {
 	}
 	return &DB{eng}, nil
 }
+
+// --- Transaction
+
+func (db *DB) RunTx(ctx context.Context, stmts ...Runner) error {
+	tx := db.eng.Driver.BeginTx(ctx)
+
+	for _, stmt := range stmts {
+		stmt, ok := stmt.(interface {
+			RunTx(context.Context, core.Transaction) error
+		})
+		if !ok {
+			_ = tx.Rollback()
+			return errors.New("runner does not support transactions")
+		}
+
+		if err := stmt.RunTx(ctx, tx); err != nil {
+			_ = tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+// --- DML Statements
 
 func (db *DB) Select(ref string, fn func(q SelectQuery, e Expr)) Statement {
 	q := &statement.SelectQueryImpl{}
@@ -48,6 +77,8 @@ func (db *DB) Delete(ref string, fn func(q DeleteQuery, e Expr)) Statement {
 	return stmt.Start(db.eng.Driver, q)
 }
 
+// --- DDL Statements
+
 func (db *DB) CreateTable(name string, fn func(q CreateTable, e Expr)) Statement {
 	q := &statement.CreateTableQueryImpl{}
 	e := &exprimpl.ExprBuilderImpl{}
@@ -56,6 +87,8 @@ func (db *DB) CreateTable(name string, fn func(q CreateTable, e Expr)) Statement
 	stmt := &statement.StatementImpl{}
 	return stmt.Start(db.eng.Driver, q)
 }
+
+// --- Connection
 
 func (db *DB) Close() error {
 	return db.eng.Driver.Close()
